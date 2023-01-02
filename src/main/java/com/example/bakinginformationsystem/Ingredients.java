0ac6d25d8a3a5fae9a901bafb94325f24fb9bbfd
Jordan Flynn <91226646+JordanFlynn@users.noreply.github.com>
2023-01-02 10:07:25 +0000

    Added Ingredients class

--- src/main/java/com/example/bakinginformationsystem/Ingredients.java
+++ src/main/java/com/example/bakinginformationsystem/Ingredients.java
@@ -1,5 +1,47 @@
 package com.example.bakinginformationsystem;
 
 //to be done
-public class Ingredients {
+public class Ingredients{
+
+    private String ingrName;
+    private String ingrDesc;
+    private int calories;
+
+    //Constructor
+    public Ingredients(String ingrName, String ingrDesc, int calories){
+        this.ingrName = ingrName;
+        this.ingrDesc = ingrDesc;
+        this.calories = calories;
+    }
+
+    //Getters+Setters
+    public String getIngrName() {
+        return ingrName;
+    }
+
+    public void setIngrName(String ingrName) {
+        this.ingrName = ingrName;
+    }
+
+    public String getIngrDesc() {
+        return ingrDesc;
+    }
+
+    public void setIngrDesc(String ingrDesc) {
+        this.ingrDesc = ingrDesc;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    //Methods
+    @Override
+    public String toString(){
+        return "Name: " + ingrName + ", Description: " + ingrDesc + ", Calories: " + calories;
+    }
 }
