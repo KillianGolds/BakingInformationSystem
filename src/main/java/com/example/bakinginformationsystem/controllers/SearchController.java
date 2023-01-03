2b36c3f1b9356de45e8c3a0e5db6e63d162ebab8
Jordan Flynn <91226646+JordanFlynn@users.noreply.github.com>
2023-01-03 16:12:49 +0000

    Search FXML basic is done. SearchController started

--- src/main/java/com/example/bakinginformationsystem/controllers/SearchController.java
+++ src/main/java/com/example/bakinginformationsystem/controllers/SearchController.java
@@ -1,5 +1,40 @@
 package com.example.bakinginformationsystem.controllers;
 
-//to be done
+import com.example.bakinginformationsystem.GenList;
+import com.example.bakinginformationsystem.Recipe;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class SearchController {
+    public static RecipeController searchControl;
+    @FXML
+    public Button viewAll;
+    @FXML
+    public Button viewBakedGoods;
+    @FXML
+    public Button viewIngredients;
+    @FXML
+    public Button viewRecipes;
+    @FXML
+    public ListView<String> searchListView;
+    @FXML
+    public GenList<String> searchList;
+    @FXML
+    public Button refreshSearchListView;
+
+    public void setViewBakedGoods(ActionEvent actionEvent) {
+    }
+
+
+
+
+    //deletes the search listview and repopulates it from the generic linked list
+    public void refreshRecipeListView(ActionEvent actionEvent) {
+        searchListView.getItems().clear(); //deletes listview content
+        searchList.iterate(getSearchListView()); //iterates over each element in the search list and adds each element to the listview.
+    }
 }
